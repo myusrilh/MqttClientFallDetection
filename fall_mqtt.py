45ace34ae83adbd9c984c1861e8d6263283f5f54
myusrilh <yusrilhasriansyah@gmail.com>
2022-07-08 13:50:24 +0700

    update logic

--- fall_mqtt.py
+++ fall_mqtt.py
@@ -4,7 +4,6 @@ import numpy as np
 import requests
 import json
 import math
-import time
 from datetime import datetime
 
 from sqlalchemy import null
@@ -16,8 +15,6 @@ class FallMQTT():
         self.roll = 0
         self.pitch = 0
         self.yaw = 0
-        # self.gyr_sen = gyr_sen
-        # self.acc_sen = 1/64
     
     def calculate_complementary(self, accRoll, accPitch, gX, gY, gZ, dTime):
         const_time = 2
@@ -25,13 +22,10 @@ class FallMQTT():
         self.roll = 0 + ((gZ*dTime)*compConst) + (accRoll*(1-compConst))
         self.pitch = 0 + ((gX*dTime)*compConst) + (accPitch*(1-compConst))
         
-#             dataframe.loc[ind,"Yaw"] = dataframe.loc[ind,"Yaw"]+ (dataframe.loc[ind,"Gy"]*d_time)
         self.yaw = 0 + (gY*dTime)
         if self.yaw < 0:
             self.yaw = self.yaw + 360.0
         
-        # self.roll = roll
-        # self.pitch = pitch
         
         return True
 
@@ -53,7 +47,6 @@ class FallMQTT():
             payload = {"Ax":[0.01],"Ay":[0.01],"Az":[0.01],"Gx":[0.1],"Gy":[0.1],"Gz":[0.1],"d_time":[0.1]}
         
         rad_to_deg = 180/np.pi
-        # d_time = 1/238
         
         c1 = np.sqrt(np.power(payload["Ax"],2) + np.power(payload["Az"],2))
         c2 = np.sqrt(np.power(payload["Ax"],2) + np.power(payload["Ay"],2) + np.power(payload["Az"],2))
@@ -61,7 +54,7 @@ class FallMQTT():
         acc_pitch =  math.atan2((payload["Ax"][0]),(np.sqrt(np.power(payload["Az"][0],2) + np.power(payload["Ay"][0],2))))*rad_to_deg
         
         dTime = payload["d_time"][0]
-        # if(self.calculate_Kalman(acc_roll,acc_pitch,payload["Gx"][0],payload["Gy"][0],payload["Gz"][0],payload["d_time"][0])):
+        
         gX = payload["Gx"][0]
         gY = payload["Gy"][0]
         gZ = payload["Gz"][0]
@@ -75,27 +68,27 @@ class FallMQTT():
         
         payload = {"C1":[c1[0]],"C2":[c2[0]],"Roll":[self.roll],"Pitch":[self.pitch],"Yaw":[self.yaw]}
         # payload = {"Ax":[payload["Ax"][0]],"Ay":[payload["Ay"][0]],"Az":[payload["Az"][0]],"Gx":[gX],"Gy":[gY],"Gz":[gZ],"C1":[c1[0]],"C2":[c2[0]]}
-        # print(payload)
+        
         post_data = {'dataset_ID': 1, 'time': json.dumps(dTime), 'payload': json.dumps(payload)}
-        # print(post_data)
         
         dt_before_req_api = datetime.utcnow().isoformat(sep=' ', timespec='milliseconds')
-        # dt_before_req_api = time.strftime('%A, %d %B %Y %H:%M:%S')
         
-        y_predict = requests.post('http://127.0.0.1:5000/falls', data=post_data)
+        # y_predict = requests.post('http://127.0.0.1:5000/falls', data=post_data)
+        y_predict = requests.post('https://falldetectionsystemapi.herokuapp.com/falls', data=post_data)
         
         dt_after_req_api = datetime.utcnow().isoformat(sep=' ', timespec='milliseconds')
-        # dt_after_req_api = time.strftime('%A, %d %B %Y %H:%M:%S')
         
-        # y_predict = requests.post('http://127.0.0.1:5000/falls', data=post_data).text
-
-        # Make array from the list
-        # y_predict = np.array(y_predict)
-        # client.publish('label/machine_learning', json.dumps({'prediction':y_predict.json()['prediction'],'seconds':y_predict.json()['seconds'],'datetime':y_predict.json()['datetime']}))
         client.publish('label/machine_learning', json.dumps({'prediction':y_predict.json()['prediction'],'dt_after_pred':y_predict.json()['dt_after_pred'],'dt_before_pred':y_predict.json()['dt_before_pred']}))
         
-        # logFile = open('log-pred.txt','a') #a = append, rw = read/write, w = write
-        # logFile.write("\n"+dt_before_req_api+", "+dt_after_req_api+", "+y_predict.json()['dt_before_pred']+", "+y_predict.json()['dt_after_pred']+", "+y_predict.json()['prediction']+", "+str(y_predict.status_code))
+        logFile = open('log-pred.txt','a') #a = append, rw = read/write, w = write
+        
+        if int(y_predict.json()['prediction']) == 0:
+            cond = "Beraktivitas normal"
+        elif int(y_predict.json()['prediction']) == 1:
+            cond = "Terjatuh!"
+        elif int(y_predict.json()['prediction']) == 2:
+            cond = "Posisi di lantai"
+        logFile.write("\n"+dt_before_req_api+", "+y_predict.json()['dt_before_pred']+", "+y_predict.json()['dt_after_pred']+","+dt_after_req_api+", "+cond+", "+str(y_predict.status_code)+", Walking")
         
         print("\nResult: ",y_predict.json())
         print("\tStatus code:",y_predict.status_code)
